18165fb88161f616b546e25a5aaf5f96b331adf0
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-12-30 07:01:33 +0530

    Update Main3.java

--- Lab-05/Main3.java
+++ Lab-05/Main3.java
@@ -6,8 +6,8 @@ public class Main3
         Scanner sc=new Scanner(System.in);
         int m=sc.nextInt();
         int n=sc.nextInt();
-        char mat[][]=new char[n][n];
-        for(int i=0;i<n;i++)
+        char mat[][]=new char[m][n];
+        for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
                 mat[i][j]=sc.next().charAt(0);
         int flag=0;
